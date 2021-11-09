7550f3cb51737550050e16a3467501a2e0329eca
Julian <jose.torres122@outlook.com>
2021-11-09 16:50:11 -0400

    Wrote some methods for Task 5

---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''Module contains BaseModel class'''
+import json
+import uuid
+from datetime import datetime
+
+
+class FileStorage:
+    '''
+    Class contains...
+    '''
+
+    __file__path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''Method returns dict __objects'''
+        return self.__objects
+    
+    def new(self, obj):
+        '''Method sets in the obj with key'''
+        self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
+
+    def save(self):
+        '''Method'''
+        new_dict = {}
+        with open (self.__file__path , "w") as f:
+            for k, v in self.__objects.items():
+                new_dict[k] = v.to_dict()
+            json.dump(new_dict, f)
+        
+    def reload(self):
+        
