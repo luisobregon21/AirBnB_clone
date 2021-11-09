6eff20c2d67b8db33f2084af3ec5403300ca3cd2
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-11-09 15:14:01 +0000

    Change order of the if statement

--- models/base_model.py
+++ models/base_model.py
@@ -13,21 +13,22 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         '''Class instantiator'''
-        if kwargs is not None:
-            if 'name' in kwargs:
-                self.name = kwargs['name']
-            if 'id' in kwargs:
-                self.id = kwargs['id']
-            if 'my_number' in kwargs:
-                self.my_number = kwargs['my_number']
-            if 'created_at' in kwargs:
-                self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            if 'update_at' in kwargs:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+        if kwargs is not None:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            if 'my_number' in kwargs:
+                self.my_number = kwargs['my_number']
+            if 'update_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            if 'name' in kwargs:
+                self.name = kwargs['name']
+     
     '''Magic Methods'''
 
     def __str__(self):
