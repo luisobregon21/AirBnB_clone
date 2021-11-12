c8774eeeef6a314af2a8b18eb004b0d4dbadb723
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-11-12 00:55:42 +0000

    Add test file for Base model

---
+++ tests/base_model_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Unittest for BaseModel Class
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    ''' Class tests BaseModel class '''
+
+
+    def test_init(self):
+        ''' Method tests the init method of base_model '''
+
+        self.test_model = BaseModel()
