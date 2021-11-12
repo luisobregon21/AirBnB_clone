e4f755e015f0a3406a927767e73a970365fa53e8
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-11-12 00:54:57 +0000

    Add a dictionary of the classes

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 ''' Module creates a unique FileStorage instance '''
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.amenity import Amenity
+from models.review import Review
 
+hbnb_classes = { 'BaseModel': BaseModel, 'City': City,
+        'User': User, 'Place': Place, 'State': State,
+        'Amenity': Amenity, 'Review': Review
+        }
 
 storage = FileStorage()
 storage.reload()
