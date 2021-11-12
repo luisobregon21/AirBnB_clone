297f330dd07ca0de97945de17e5b9818780cb611
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-11-12 00:54:15 +0000

    Add the create method for console

--- console.py
+++ console.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 ''' Module contains the entry point of the command interpreter '''
+''' REMEMBER TO DELETE MESSAGES '''
 
 import cmd
-#from models import storage
-
+from models import hbnb_classes, storage
 
 class HBNBCommand(cmd.Cmd):
     ''' Class holds commands that a user can use in console '''
 
     intro = 'Welcome to HELL. Type help or ? to list commands.\n'
-    prompt = '(CBRON) '
+    prompt = '(hbnb) '
     file = None
 
-    ''' ¡¡¡¡ in the works !!!!
     def do_create(self, line):
+        ''' Creates an instance of the specified class'''
         if len(line) == 0:
             print("** class name missing ** ")
+        elif line in hbnb_classes:
+            n_class = hbnb_classes[line]()
+            print(n_class.id)
+            storage.new(n_class)
+            storage.save()
         else:
-            storage.new(line)
-            storage.save() '''
+            print("** class doesn't exist **")
+
 
     def do_quit(self, line):
         ''' Command exists program: GOOD BYE'''
@@ -29,6 +34,10 @@ class HBNBCommand(cmd.Cmd):
         ''' Command exists program: See you soon. '''
         print('BYE BUDDY')
         return True
+
+    def Enter(self):
+        ''' When there is no input '''
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
