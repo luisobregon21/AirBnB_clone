fe650000e259d4db55ec252537ff67b7de85310d
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-11-12 20:23:07 +0000

    Add show, all and destroy commands for console... also imported class modules to engine

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 ''' REMEMBER TO DELETE MESSAGES '''
 
 import cmd
+from types import new_class
 from models import hbnb_classes, storage
 
 class HBNBCommand(cmd.Cmd):
@@ -12,28 +13,87 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
     file = None
 
-    def do_create(self, line):
+    def do_create(self, arg):
         ''' Creates an instance of the specified class'''
-        if len(line) == 0:
+        if len(arg) == 0:
             print("** class name missing ** ")
-        elif line in hbnb_classes:
-            n_class = hbnb_classes[line]()
+        elif arg in hbnb_classes:
+            n_class = hbnb_classes[arg]()
             print(n_class.id)
             storage.new(n_class)
             storage.save()
         else:
             print("** class doesn't exist **")
 
+    def do_all(self, arg):
+        ''' Prints all instances based or not on the class name.'''
+        all_objs = storage.all()
+        if len(arg) == 0:
+            for obj_id in all_objs.keys():
+                obj = all_objs[obj_id]
+                print(obj)
+        else:
+            if arg not in hbnb_classes.keys():
+                print("** class doesn't exist **")
+            else:
+                leng = len(arg)
+                for key, val in all_objs.items():
+                    if key[0:leng] == arg:
+                        print(val)
+
+    def do_show(self, arg):
+        ''' Shows instance based on the class'''
+        class_list = arg.split(" ") 
+        try:
+            key = class_list[0] + "." + class_list[1]
+        except:
+            pass
+
+        if not arg:
+            print("** class name missing ** ")
+        else:
+            if class_list[0] in hbnb_classes:
+                if len(class_list) < 2:
+                    print("** instance id missing **")
+                elif key not in storage.all().keys():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[key])
+            else:
+                print("** class doesn't exist **")
 
 
-    def do_quit(self, line):
+    def do_destroy(self, arg):
+        ''' Deletes instance based on the class'''
+        class_list = arg.split(" ") 
+        try:
+            key = class_list[0] + "." + class_list[1]
+        except:
+            pass
+
+        if not arg:
+            print("** class name missing ** ")
+        else:
+            if class_list[0] in hbnb_classes:
+                if len(class_list) < 2:
+                    print("** instance id missing **")
+                elif key not in storage.all().keys():
+                    print("** no instance found **")
+                else:
+                    del storage.all()[key]
+            else:
+                print("** class doesn't exist **")
+
+
+    ''' Basic Commands below'''
+    def do_quit(self, arg):
         ''' Command exists program: GOOD BYE'''
         print('Thank you for being you')
         return True
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         ''' Command exists program: See you soon. '''
-        print('BYE BUDDY')
+        print('BYE BUDDY: Do not be a fucker')
         return True
 
     def Enter(self):
