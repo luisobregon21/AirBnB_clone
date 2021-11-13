f0c0cf862bcb471f439de61ce19e0994397c73d5
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-11-13 15:29:39 +0000

    Add test

--- tests/base_model_test.py
+++ tests/base_model_test.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 """
-Unittest for BaseModel Class
+Unittest module for BaseModel Class
 """
 import unittest
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class TestBaseModel(unittest.TestCase):
-    ''' Class tests BaseModel class '''
+    """Class test BaseModel class"""
+    @classmethod
+    def setUpClass(cls):
+        cls._connection = BaseModel()
 
     def test_init(self):
-        ''' Method tests the init method of base_model '''
-        self.test_model = BaseModel()
+        """test init method of BaseModel"""
+        test_model = BaseModel()
+        message = "Object is not instance of class"
+        self.assertIsInstance(test_model, BaseModel, message)
+
+
+if __name__ == "__main__":
+    unittest.main()
