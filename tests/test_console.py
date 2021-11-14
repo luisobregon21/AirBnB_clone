11495190625a5bb7219d14a7abae2541a3c7a4c3
Julian <jose.torres122@outlook.com>
2021-11-14 17:55:29 -0400

    Push all unittests necessary

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Unittest module for Console
+"""
+import unittest
+
+
+class TestUserClass(unittest.TestCase):
+    """Class test Console"""
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,22 +2,22 @@
 """
 Unittest module for User Class
 """
+from datetime import datetime
 import unittest
 from unittest.case import _AssertRaisesContext
 from models.base_model import BaseModel
-from test_base_model import BaseModel
-from datetime import datetime
-from models.user import User
 from models.engine.file_storage import FileStorage
+from test_base_model import BaseModel
+from models.base_model import Place
 
 
-class TestUserClass(unittest.TestCase):
-    """Class test User class"""
+class TestBaseModel(unittest.TestCase):
+    """Class test BaseModel class"""
 
     def setUp(self):
         ''' create two base models'''
-        self.A_user = User()
-        self.B_user = User()
+        self.A_user = Place()
+        self.B_user = Place()
 
     def test_str_is_str(self):
         ''' Test __str__ method '''
@@ -25,12 +25,12 @@ class TestUserClass(unittest.TestCase):
 
     def test_str_content(self):
         """checks content of __str__"""
-        self.assertIn("[BaseModel]", self.B_user.__str__())
+        self.assertIn("[Place]", self.A_user.__str__())
 
     def test_Init(self):
         ''' Checks if Init works'''
-        self.assertIsInstance(self.A_user, User())
-        self.assertIsInstance(self.B_user, User())
+        self.assertIsInstance(self.A_user, Place)
+        self.assertIsInstance(self.B_user, Place)
 
     def test_Id(self):
         ''' Test if each instance have an unique id '''
@@ -44,11 +44,11 @@ class TestUserClass(unittest.TestCase):
     def test_date_is_date(self):
         '''test if dates are datetime type'''
         self.assertIsInstance(self.A_user.created_at, datetime)
-        self.assertIsInstance(self.A_user.updated_at, datetime)
+        self.assertIsInstance(self.B_user.updated_at, datetime)
 
     def test_date_update(self):
         '''test if datetimes updates correctly'''
-        self.A_user.save()
+        self.A_base_model.save()
         self.assertNotEqual(self.A_user.created_at,
                             self.A_user.updated_at)
 
@@ -59,16 +59,6 @@ class TestUserClass(unittest.TestCase):
     def test_to_dict_has_key(self):
         """test if to_dict works"""
         self.assertIn("created_at", self.A_user.to_dict())
-
-    def test_add_attribute(self):
-        """test adding attribute"""
-        self.A_user.name = "pepe"
-        self.assertEqual(self.A_user.name, "pepe")
-
-    def test_kwarg(self):
-        objdict = self.A_user.to_dict()
-        self.kwarginstance = BaseModel(**objdict)
-        self.assertIsInstance(self.kwarginstance, User())
 
 
 if __name__ == "__main__":
